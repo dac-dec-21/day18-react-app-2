7c078a3376c08a2dff4ed45e455778e861091ef9
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-03 18:31:35 +0530

    b1.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+/**
+ * App => Captial
+ * JSX => Javascript XML.
+ * @returns JSX => XML
+ */
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HelloWorld />
+      <HelloWorld />
     </div>
   );
 }
 
-export default App;
+/**
+ * HTML => PRE DEFINED TAG
+ * REACT => USER DEFINED TAG => COMPONENT
+ *
+ * <div></div>
+ * <HelloWorld></HelloWorld>
+ * @returns
+ */
+export function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello Cdac</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
+        voluptates inventore est assumenda reiciendis repellendus facere
+        exercitationem explicabo similique. Optio voluptatem ex expedita sit
+        molestias perferendis quod neque libero placeat!
+      </p>
+    </div>
+  );
+}
