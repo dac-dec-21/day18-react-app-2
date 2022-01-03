e10afdcbe128aa44c4e74e7e29d86f7b4e44286a
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-03 19:24:53 +0530

    shooping.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 export default function App() {
+  let list = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
   return (
     <div className="container-fluid">
       <Header />
 
       <div className="row mt-1">
-        <div className="col-sm-12 col-md-4 mb-1">
-          <MyCard />
-        </div>
-        <div className="col-sm-12 col-md-4 mb-1">
-          <MyCard />
-        </div>
-        <div className="col-sm-12 col-md-4 mb-1">
-          <MyCard />
-        </div>
-        <div className="col-sm-12 col-md-4 mb-1">
-          <MyCard />
-        </div>
-
-        <div className="col-sm-12 col-md-4 mb-1">
-          <MyCard />
-        </div>
+        {list.map(() => (
+          <div className="col-sm-12 col-md-4 mb-1">
+            <MyCard />
+          </div>
+        ))}
       </div>
     </div>
   );
@@ -27,16 +18,18 @@ export default function App() {
 
 function Header() {
   return (
-    <div className="row bg-dark text-light p-3">
+    <div className="row bg-dark text-light p-3 sticky-top">
       <div className="col h3">React App</div>
     </div>
   );
 }
 
 function MyCard() {
+  let cardTitle = "Card Titleee";
+
   return (
     <div className="card">
-      <div className="card-header">Card Header</div>
+      <div className="card-header">{cardTitle}</div>
       <div className="card-body">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione nulla
         earum, iure magnam at assumenda qui et itaque, modi totam mollitia
@@ -44,9 +37,14 @@ function MyCard() {
         officia necessitatibus tenetur architecto. itaque, modi totam mollitia
         officia necessitatibus tenetur architecto.
         <input
-          className="btn btn-sm btn-outline-secondary w-100 mt-2"
+          className="btn btn-sm btn-outline-secondary w-50 mt-2 "
           type="button"
           value="Buy Now"
+        />
+        <input
+          className="btn btn-sm btn-outline-secondary w-50 mt-2"
+          type="button"
+          value="Add To Cart"
         />
       </div>
     </div>
