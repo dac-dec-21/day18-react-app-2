c1a320ae1e5a8dcb8bb75f435a33dbc6152826ce
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-03 18:45:48 +0530

    b3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 /**
- * App => Captial
- * JSX => Javascript XML.
- * @returns JSX => XML
- *
- * <App></App>
- * <App />
+ * Main Function.
  */
 export default function App() {
   return (
     <div>
       <HelloWorld></HelloWorld>
+      <HelloUniverse />
       <HelloWorld />
+      <HelloUniverse />
     </div>
   );
 }
@@ -20,7 +17,7 @@ export default function App() {
  * <HelloWorld />
  * @returns
  */
-export function HelloWorld() {
+function HelloWorld() {
   return (
     <div>
       <h1>Hello World</h1>
@@ -29,6 +26,25 @@ export function HelloWorld() {
         voluptates inventore est assumenda reiciendis repellendus facere
         exercitationem explicabo similique. Optio voluptatem ex expedita sit
         molestias perferendis quod neque libero placeat!
+      </p>
+    </div>
+  );
+}
+
+function HelloUniverse() {
+  return (
+    <div>
+      <h1>Hello Universe</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        aliquam velit fuga aliquid dolor. Voluptas, maiores nesciunt facilis
+        obcaecati amet suscipit nemo dignissimos eius, commodi totam, incidunt
+        expedita. Explicabo, soluta.
+        <hr />
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus,
+        voluptate. Iusto at excepturi cupiditate. Placeat dolore quisquam
+        dolores beatae soluta asperiores iure tenetur repellat. Iure beatae odio
+        amet enim aliquam.
       </p>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { HelloWorld } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HelloWorld />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
